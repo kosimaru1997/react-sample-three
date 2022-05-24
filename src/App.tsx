dc3bb9cac96c9672d8c4759f3bd83cae8bce8489
kosimaru1997 <kosimaru1997@gmail.com>
2022-05-25 03:12:12 +0900

    fix lint error and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
-import Home from './Home';
+import { Home } from './Home';
 
-const App: FC = () => (
+export const App: FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
   </Routes>
 );
-
-export default App;
